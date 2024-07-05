515b1a14af2e639f90403e2099eb29f2e86af338
shreyaa-1 <104620660+sh-reya1@users.noreply.github.com>
2024-07-05 10:58:26 +0530

    latest

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-
+// const cors = require('cors');
+const mongoose = require("mongoose");
+const app = express();
+// require('dotenv').config();
+// const BASE_URL = process.env.BASE_URL;
+// console.log(BASE_URL);
+const uri ="mongodb+srv://shreyaa2802:<password>@cluster0.iji1v8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const app = express();
 const PORT = 5001; // Change port number
+app.use(express.json());
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => console.log(err));
 
 // Middleware
 app.use(bodyParser.json());
